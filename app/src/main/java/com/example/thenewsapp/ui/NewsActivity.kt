2c199c1f7859cd7aa22f80dd7b1cb23e03a7aa5a
SK Nur Ahmad <sknurahmad2@gmail.com>
2024-08-16 01:20:00 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/thenewsapp/ui/NewsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.thenewsapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.thenewsapp.R
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+
+    }
+}
---
+++ app/src/main/java/com/example/thenewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.thenewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "67da24d68fbd4c768f8132b65d6381b7"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = "500L"
+        const val QUERY_PAGE_SIZE = "20"
+    }
+}
