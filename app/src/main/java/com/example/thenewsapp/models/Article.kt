137c99d3c1ced9877f1ae8b899a408e389c7e253
SK Nur Ahmad <sknurahmad2@gmail.com>
2024-08-26 19:41:07 +0530

    make all the variable nullable to prevent issue occuring null value

--- app/src/main/java/com/example/thenewsapp/models/Article.kt
+++ app/src/main/java/com/example/thenewsapp/models/Article.kt
@@ -8,12 +8,12 @@ import java.io.Serializable
 data class Article(
     @PrimaryKey(autoGenerate = true)
     var id: Int? = null,
-    val author: String,
-    val content: String,
-    val description: String,
-    val publishedAt: String,
-    val source: Source,
-    val title: String,
-    val url: String,
-    val urlToImage: String
+    val author: String? = null, // Allow null for author
+    val content: String? = null, // Allow null for content
+    val description: String? = null, // Allow null for description
+    val publishedAt: String? = null, // Allow null for publishedAt
+    val source: Source? = null, // Allow null for source
+    val title: String? = null, // Allow null for title
+    val url: String? = null, // Allow null for url
+    val urlToImage: String? = null // Allow null for urlToImage
 ) : Serializable
