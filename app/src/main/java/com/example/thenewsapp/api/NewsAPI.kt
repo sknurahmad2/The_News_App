3272aa07944e3bc174f6b5ea4a3b54c237360337
SK Nur Ahmad <sknurahmad2@gmail.com>
2024-08-26 19:44:01 +0530

    the deafult county code changed in to us

--- app/src/main/java/com/example/thenewsapp/api/NewsAPI.kt
+++ app/src/main/java/com/example/thenewsapp/api/NewsAPI.kt
@@ -10,7 +10,7 @@ interface NewsAPI {
     @GET("v2/top-headlines")
     suspend fun getHeadlines(
         @Query("country")
-        countryCode : String = "in" ,
+        countryCode : String = "us" ,
         @Query("page")
         pageNumber : Int = 1,
         @Query("apiKey")
