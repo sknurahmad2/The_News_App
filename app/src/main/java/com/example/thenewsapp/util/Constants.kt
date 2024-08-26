a00d6ace01c8584fae9a1e66f1fdc7a0c88187ab
SK Nur Ahmad <sknurahmad2@gmail.com>
2024-08-26 19:45:33 +0530

    changed the variable string to int search_news_time_delay and query_page_size

--- app/src/main/java/com/example/thenewsapp/util/Constants.kt
+++ app/src/main/java/com/example/thenewsapp/util/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
     companion object{
         const val API_KEY = "67da24d68fbd4c768f8132b65d6381b7"
         const val BASE_URL = "https://newsapi.org/"
-        const val SEARCH_NEWS_TIME_DELAY = "500L"
-        const val QUERY_PAGE_SIZE = "20"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
