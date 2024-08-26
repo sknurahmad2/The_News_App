1454a892e9465975761dca94170d29ef9e10d2f0
SK Nur Ahmad <sknurahmad2@gmail.com>
2024-08-26 13:04:48 +0530

    Article Fragment modified sucessfully

--- app/src/main/java/com/example/thenewsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/thenewsapp/ui/fragments/ArticleFragment.kt
@@ -1,20 +1,42 @@
 package com.example.thenewsapp.ui.fragments
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
+import androidx.navigation.fragment.navArgs
 import com.example.thenewsapp.R
+import com.example.thenewsapp.databinding.FragmentArticleBinding
+import com.example.thenewsapp.ui.NewsActivity
+import com.example.thenewsapp.ui.NewsViewModel
+import com.google.android.material.snackbar.Snackbar
 
-class ArticleFragment : Fragment() {
+class ArticleFragment : Fragment(R.layout.fragment_article) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_article, container, false)
+    lateinit var newsViewModel : NewsViewModel
+    val args : ArticleFragmentArgs by navArgs()
+    lateinit var binding : FragmentArticleBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentArticleBinding.bind(view)
+
+        newsViewModel = (activity as NewsActivity).newsViewModel
+        val article = args.article
+
+        binding.webView.apply {
+            webViewClient = WebViewClient()
+            article.url?.let {
+                loadUrl(it)
+            }
+        }
+
+        binding.fab.setOnClickListener {
+            newsViewModel.addToFavourites(article)
+            Snackbar.make(view,"Article added to favourites",Snackbar.LENGTH_SHORT).show()
+        }
     }
-
 }
