822e8c2101cd00f630fb7b1cb9714ad3fe9e8088
SK Nur Ahmad <sknurahmad2@gmail.com>
2024-08-27 18:19:18 +0530

    webview loading news function modified

--- app/src/main/java/com/example/thenewsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/thenewsapp/ui/fragments/ArticleFragment.kt
@@ -1,17 +1,21 @@
 package com.example.thenewsapp.ui.fragments
 
 import android.content.res.Configuration
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebSettings
+import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import com.example.thenewsapp.R
 import com.example.thenewsapp.databinding.FragmentArticleBinding
+import com.example.thenewsapp.models.Article
 import com.example.thenewsapp.ui.NewsActivity
 import com.example.thenewsapp.ui.NewsViewModel
 import com.google.android.material.snackbar.Snackbar
@@ -21,16 +25,38 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
     private lateinit var newsViewModel : NewsViewModel
     val args : ArticleFragmentArgs by navArgs()
     lateinit var binding : FragmentArticleBinding
+    lateinit var progressBar: ProgressBar
+    lateinit var article: Article
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentArticleBinding.bind(view)
         newsViewModel = ViewModelProvider(requireActivity())[NewsViewModel::class.java]
         newsViewModel = (activity as NewsActivity).newsViewModel
-        val article = args.article
+        article = args.article
+        progressBar = binding.progressBar
 
+        setUpWebView()
+
+        binding.fab.setOnClickListener {
+            newsViewModel.addToFavourites(article)
+            Snackbar.make(view,"Article added to favourites",Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setUpWebView() {
         binding.webView.apply {
-            webViewClient = WebViewClient()
+            webViewClient = object :WebViewClient(){
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                    super.onPageStarted(view, url, favicon)
+                    binding.progressBar.visibility = View.VISIBLE
+                }
+
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    binding.progressBar.visibility = View.GONE
+                }
+            }
             article.url?.let {url ->
                 settings.apply {
                     javaScriptEnabled = true
@@ -44,11 +70,6 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
                 }
                 loadUrl(url)
             }
-        }
-
-        binding.fab.setOnClickListener {
-            newsViewModel.addToFavourites(article)
-            Snackbar.make(view,"Article added to favourites",Snackbar.LENGTH_SHORT).show()
         }
     }
 }
